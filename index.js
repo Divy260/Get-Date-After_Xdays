4b4d875e52a6b217b2f792d1e3ff063ea2db7f3d
Divy260 <divya.matavalam828@gmail.com>
2021-12-06 04:26:40 +0000

    syntax correction

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const addDays = require("date-fns/addDays");
 
 const dateAfterXDays = (days) => {
-  const newDate = addDays(new Date(2020, 07, 22), days);
+  const newDate = addDays(new Date(2020, 7, 22), days);
   return (
     `${newDate.getDate()}` -
-    `${newDate.getMonth()}` -
+    `${newDate.getMonth() + 1}` -
     `${newDate.getFullYear()}`
   );
 };
